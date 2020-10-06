7fa0c9c6fa8621bdcfed849c0303424e34d344e8
Radi Barq <rbarq@restaurant365.com>
2020-10-06 16:08:23 +0300

    - First api release

---
+++ config/services/httpService.js
@@ -0,0 +1,20 @@
+import axios from "axios"
+import { toast } from 'react-toastify'
+axios.interceptors.response.use(null, error => {
+    const isExpected = 
+    error.response && 
+    error.response.status >= 400 
+    && error.response.status < 500
+    if (!isExpected) {
+        console.log('Logging the error', error)
+        toast.error("An unexpected error occurred.")
+    }
+    return Promise.reject(error)
+})
+
+export default {
+    get: axios.get,
+    post: axios.post,
+    put: axios.put,
+    delete: axios.delete
+}
